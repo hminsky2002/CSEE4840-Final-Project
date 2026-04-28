d7cae609d8ffbb037518feda4b0b3a57fdb8ef16
Harry Minsky <harry.minsky@gmail.com>
2026-04-28 19:23:37 -0400

    try some alsa slop

--- sw/midi.c
+++ sw/midi.c
@@ -1,49 +1,21 @@
-/*
- *
- * CSEE 4840 Final Project
- *
- * Name/UNI: Henry Minsky (hm3121), Opalina Khanna (ok2373), Sunny Fang (yf2610)
- */
-#include "midi.h"
-#include "usbmidi.h"
+#include <alsa/asoundlib.h>
 #include <stdio.h>
-#include <stdint.h>
 
-struct libusb_device_handle *midi;
-uint8_t endpoint_address;
+int main() {
+    snd_seq_t *seq_handle;
+    snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_INPUT, 0);
+    snd_seq_set_client_name(seq_handle, "My MIDI Receiver");
+    int port_id = snd_seq_create_simple_port(seq_handle, "Input Port",
+                  SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
+                  SND_SEQ_PORT_TYPE_MIDI_GENERIC);
 
-struct libusb_device_handle *midi_open(uint8_t *endpoint_out) {
-    midi = open_midi(endpoint_out);
-    endpoint_address = *endpoint_out;
-    return midi;
-}
-
-int midi_read(struct libusb_device_handle *midi,
-                uint8_t endpoint_address,
-                midi_event_t *evt) {
-    uint8_t buf[4];
-    int transferred;
-    int r = libusb_bulk_transfer(midi, endpoint_address,
-        buf, sizeof(buf),
-        &transferred, 500000);
-    if (r < 0) return r;
-
-    evt->cable      = buf[0];
-    evt->status     = buf[1];
-    evt->note       = buf[2];
-    evt->velocity   = buf[3];
-
-    printf("%02x %02x %02x %02x\n",
-        evt->cable,
-        evt->status,
-        evt->note,
-        evt->velocity
-    );
-
-  return 0; 
-}
-
-void midi_close(struct libusb_device_handle *midi) {
-    libusb_close(midi);
-    libusb_exit(NULL);
+    while (1) {
+        snd_seq_event_t *ev;
+        snd_seq_event_input(seq_handle, &ev);
+        if (ev->type == SND_SEQ_EVENT_NOTEON) {
+            printf("Note On: %d\n", ev->data.note.note);
+        }
+        snd_seq_free_event(ev);
+    }
+    return 0;
 }
