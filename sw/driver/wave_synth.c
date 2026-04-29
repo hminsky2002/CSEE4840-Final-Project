7e71cf44d0987709f6bca5da6b2cbd075b00c8b7
hminsky2002 <harry.minsky@gmail.com>
2026-04-28 22:53:27 -0400

    makefile patch

--- sw/driver/wave_synth.c
+++ sw/driver/wave_synth.c
@@ -2,6 +2,7 @@
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
