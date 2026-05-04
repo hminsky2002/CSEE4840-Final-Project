d4fd58f7cf26d429993511703d4796f77db2caed
Harry Minsky <harry.minsky@gmail.com>
2026-05-04 19:37:57 -0400

    add c code for loading file into fpga memory

---
+++ sw/wavetable.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "fpga_bridge.h"
+#include "wavetable.h"
+
+int load_wavetable_bin(const char *path, peripheral *lw_bus){
+    FILE *f = fopen(path,"rb");
+
+    if(!f){
+        perror("load_wavetable_bin: fopen");
+        return -1;
+    }
+
+    int16_t slot_buf[TABLE_SIZE];
+    int slot = 0;
+
+    while(slot < NUM_TABLE_SLOTS){
+        size_t tableslot = fread(slot_buf, sizeof(int16_t),TABLE_SIZE, f);
+
+        if(tableslot == 0){
+            break;
+        }
+
+        if (tableslot != TABLE_SIZE){
+            fprintf(stderr,"wavetable binary file is malformed, wrong number of samples read");
+            fclose(f);
+            return -1;
+        }
+        fpga_load_slot(lw_bus,slot,slot_buf,TABLE_SIZE);
+        printf("slot %d: loaded\n", slot);
+        slot++;
+    }
+    fclose(f);
+
+    return slot;
+}
